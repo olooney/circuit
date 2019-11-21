12239fc12bb6856646f085a8c32f6a1b03f0a9a4
Oran Looney <olooney@gmail.com>
2019-11-20 22:06:40 -0600

    Register and Bus classes

--- circuit/kernel.py
+++ circuit/kernel.py
@@ -23,10 +23,10 @@ class Wire:
     def value(self, value):
         if self._value is not None:
             if value != self._value:
-                raise("Wire set to conflicting value.")
+                raise WireError("Wire set to conflicting value.")
         if value is True or value is False:
             self._value = value
-            self.propogate()
+            self.propagate()
         else:
             raise WireError("Use reset() to clear wire.")
 
@@ -36,15 +36,89 @@ class Wire:
             for component in self.downstream_components:
                 component.reset()
 
-    def propogate(self):
+    def propagate(self):
         for component in self.downstream_components:
-            component.propogate()
+            component.propagate()
 
     def connect(self, component):
         self.downstream_components.append(component)
 
 
+class Bus(Wire):
+    """
+    A bus is simply a bundle of parallel wires. It implements the
+    Aggregate pattern with respect to Wire, and supports all the same
+    reset(), propagate(), and connect() methods, delegating to each
+    constituent wire in each case.
+    
+    It's value is an unsigned integer where each bit corresponds with one wire,
+    with the most significant bit being the first wire, and the least
+    significant bit being the last wire.
+    """
+
+    def __init__(self, wires):
+        """
+        Initialize either from an iterable of wires
+        or from an integer representing the number
+        of wires in the bus. If any of the elements
+        of the iterable are None, they will be 
+        initialized as new Wire objects.
+        """
+        try:
+            def as_wire(wire):
+                if wire is None:
+                    return Wire()
+                if type(wire) == bool:
+                    return Wire(wire)
+                return wire
+
+            self.wires = tuple( as_wire(w) for w in wires )
+        except TypeError:
+            self.wires = tuple( Wire() for w in range(wires) )
+
+    def __len__(self):
+        return len(self.wires)
+
+    def __getitem__(self, index):
+        return self.wires[index]
+
+    def __iter__(self):
+        yield from self.wires
+
+    def propagate(self):
+        for wire in self.wires:
+            wire.propagate()
+
+    def reset(self):
+        for wire in self.wires:
+            wire.reset()
+
+    def connect(self, component):
+        for wire in self.wires:
+            wires.connect(component)
+
+    @property
+    def value(self):
+        value = 0
+        for wire in self.wires:
+            value <<= 1
+            if wire.value is True:
+                value += 1
+        return value
+
+    @value.setter
+    def value(self, value):
+        for wire in reversed(self.wires):
+            wire.value = bool(value % 2)
+            value >>= 1
+
+
 class Component:
+    """
+    A component is simply a number of input and output
+    pins (Wires) which internally are wired together
+    from simplier constituent Components.
+    """
     def __init__(self):
         self.inputs = []
         self.outputs = []
@@ -62,7 +136,7 @@ class Component:
         self.outputs.append(out)
         return out
 
-    def propogate(self):
+    def propagate(self):
         pass
 
     def reset(self):
@@ -71,13 +145,19 @@ class Component:
 
 
 class NAND(Component):
+    """
+    NAND is only "atomic" Component necessary to implement all of combinational
+    logic. It has two input pins `a` and `b`, and an output pin `out`. The value
+    of `out` will be set as soon as it can be determined from `a` and `b`. The
+    output value will be False if and only if both inputs are True.
+    """
     def __init__(self, a, b, out=None):
         super().__init__()
         self.a = self.input(a)
         self.b = self.input(b)
         self.out = self.output(out)
 
-    def propogate(self):
+    def propagate(self):
         if self.out.value is not None:
             return
 
@@ -87,3 +167,37 @@ class NAND(Component):
             self.out.value = True
         elif self.a.value is True and self.b.value is True:
             self.out.value = False
+
+
+class Register(Component):
+    """
+    The Register is the only "stateful" Component necessary to implement all
+    of sequential logic. It's output is always determined from previous clock
+    cycles.
+    """
+    def __init__(self, inp, enable, out=None):
+        super().__init__()
+        self.inp = self.input(inp)
+        self.enable = self.input(enable)
+        self.out = self.output(out)
+
+        self.state = False
+        self.next_state = None
+
+    def propagate(self):
+        # capture the next state if enabled
+        if self.enable.value is True and self.inp.value is not None:
+            self.next_state = self.inp.value
+
+        # always propogate the current value
+        if self.out.value is None:
+            self.out.value = self.state
+
+    def reset(self):
+        # update internal state if appropriate
+        if self.next_state is not None:
+            self.state = self.next_state
+            self.next_state = None
+
+        super().reset()
+
---
+++ test/test_kernel.py
@@ -0,0 +1,112 @@
+import unittest 
+from circuit.kernel import Wire, Bus, Register, NAND
+from circuit.kernel import WireError
+
+class TestWire(unittest.TestCase):
+    def test_wire(self):
+        wire = Wire()
+        self.assertIs(wire.value, None)
+        wire.value = True
+        self.assertIs(wire.value, True)
+        with self.assertRaises(WireError):
+            wire.value = False
+        wire.reset()
+        self.assertIs(wire.value, None)
+        wire.value = False
+        self.assertIs(wire.value, False)
+
+
+class TestBus(unittest.TestCase):
+    def test_init_from_wires(self):
+        ones = Wire(True)
+        twos = Wire(False)
+        fours = Wire(False)
+        eights = Wire(True)
+        bus = Bus([ones, twos, fours, eights])
+        self.assertEqual(len(bus), 4)
+        self.assertEqual(bus.value, 9)
+
+    def test_init_from_bool(self):
+        bus = Bus([False, True, False, True])
+        self.assertEqual(len(bus), 4)
+        self.assertEqual(bus.value, 5)
+
+        bus = Bus([True, False, True, False])
+        self.assertEqual(len(bus), 4)
+        self.assertEqual(bus.value, 10)
+
+        bus = Bus([True, False, False, False, False])
+        self.assertEqual(len(bus), 5)
+        self.assertEqual(bus.value, 16)
+
+        bus = Bus([False, False, True])
+        self.assertEqual(len(bus), 3)
+        self.assertEqual(bus.value, 1)
+
+    def test_init_from_length(self):
+        bus = Bus(4)
+        self.assertEqual(len(bus), 4)
+        self.assertEqual(bus.value, 0)
+        for wire in bus:
+            wire.value = True
+        self.assertEqual(bus.value, 15)
+
+    def test_set_value(self):
+        bus = Bus(4)
+        bus.value = 5
+        self.assertEqual(bus[0].value, False)
+        self.assertEqual(bus[1].value, True)
+        self.assertEqual(bus[2].value, False)
+        self.assertEqual(bus[3].value, True)
+
+        bus.reset()
+
+        bus.value = 14
+        self.assertEqual(bus[0].value, True)
+        self.assertEqual(bus[1].value, True)
+        self.assertEqual(bus[2].value, True)
+        self.assertEqual(bus[3].value, False)
+
+
+class TestRegister(unittest.TestCase):
+    def test_stateful(self):
+
+        inp, enable = Wire(), Wire()
+        register = Register(inp=inp, enable=enable)
+        out = register.out
+        
+        self.assertIs(out.value, None)
+        register.propagate()
+        self.assertIs(out.value, False)
+        
+        inp.value = True
+        enable.value = True
+        self.assertIs(out.value, False)
+
+        inp.reset()
+        enable.reset()
+
+        self.assertIs(out.value, None)
+
+        inp.value = False
+        enable.value = False
+
+        self.assertIs(out.value, True)
+
+        inp.reset()
+        enable.reset()
+
+        self.assertIs(out.value, None)
+
+        inp.value = False
+        enable.value = True
+
+        self.assertIs(out.value, True)
+
+        inp.reset()
+        enable.reset()
+        register.propagate()
+
+        self.assertIs(out.value, False)
+
+# TODO: TestNAND
